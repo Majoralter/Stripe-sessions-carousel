e8eaa51cd00f7789a741963410bb048869fa9db8
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-04-18 16:16:44 +0100

    Updated Sass file

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ newFunction()
 function newFunction() {
   newImgArray[1].classList.add("active")
 
-  newImgArray.forEach(item => item.addEventListener("click", () =>{
-    for(let item of newImgArray){
-      item.classList.remove("active")
-    }
-    item.classList.add("active")
+  sliderLogic()
 
-  }))
+  function sliderLogic() {
+    newImgArray.forEach(item => item.addEventListener("click", () => {
+      for (let item of newImgArray) {
+        item.classList.remove("active")
+      }
+      item.classList.add("active")
+
+    }))
+  }
 }
