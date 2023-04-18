9b065dc4bd2aa4eb723e78f53957e0f055f50a67
altermajor <118938444+Majoralter@users.noreply.github.com>
2023-04-18 16:09:08 +0100

    Added layout and functionality

---
+++ script.js
@@ -0,0 +1,17 @@
+const itemsImg = document.querySelectorAll("#carousel-img")
+
+let newImgArray = [...itemsImg]
+
+newFunction()
+
+function newFunction() {
+  newImgArray[1].classList.add("active")
+
+  newImgArray.forEach(item => item.addEventListener("click", () =>{
+    for(let item of newImgArray){
+      item.classList.remove("active")
+    }
+    item.classList.add("active")
+
+  }))
+}
